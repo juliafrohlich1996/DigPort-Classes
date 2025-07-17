1603368f85166f9883a72a096a416675284486a3
Frohlich <j.frohlich@sap.com>
2025-07-17 15:44:26 -0300

    aula 4 - conceitosBasicos

---
+++ conceitosBasicos aula 4/conceitosBasicos.js
@@ -0,0 +1,19 @@
+let idade = 28;
+let nome = "Júlia";
+let Ativo = true;
+let nota;
+let cidade = null;
+
+console.log(idade)
+console.log(nome)
+console.log(Ativo)
+console.log(nota)
+console.log(cidade)
+
+let nome1 = "Maria"
+var nome2 = "Maria"
+
+let nomeCliente = "Joao"
+let nomecLiente = "Maria"
+
+console.log(nome, nomeCliente, nomecLiente)
